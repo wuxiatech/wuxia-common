7c1310851bcd517187fb36d3a5f0828e7d9b5273
songlin.li <songlin.li@outlook.com>
2017-08-11 18:47:25 +0800

    优化

---
+++ src/main/java/cn/wuxia/common/web/httpclient/HttpClientMethod.java
@@ -0,0 +1,12 @@
+/*
+* Created on :2017年8月9日
+* Author     :songlin
+* Change History
+* Version       Date         Author           Reason
+* <Ver.No>     <date>        <who modify>       <reason>
+* Copyright 2014-2020 wuxia.gd.cn All right reserved.
+*/
+package cn.wuxia.common.web.httpclient;
+public enum HttpClientMethod {
+    GET, DELETE,POST, PUT;
+}
